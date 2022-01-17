598256ca3b75e39ea4eda4990364e8441d7da2ab
Aadya-9 <82094543+Aadya-9@users.noreply.github.com>
2022-01-17 15:14:06 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+function startClassification(){
+    navigator.mediaDevices.getUserMedia({audio:true});
+    classifier=ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/Wz-SXHP-Z/model.json",modelReady);
+}
+function modelReady(){
+    classifier.classify(gotResults);
+
+}
+function gotResults(error,results){
+if(error){
+    console.error(error);
+}
+else{
+    console.log(results);
+    r= Math.floor(Math.random()*255)+1;
+    g= Math.floor(Math.random()*255)+1;
+    b= Math.floor(Math.random()*255)+1; 
+document.getElementById("result_label").innerHTML="I Can Hear-"+results[0].label;
+document.getElementById("result_confidence").innerHTML="Acurracy-"+(results[0].confidence*100).toFixed(2)+"%";
+document.getElementById("result_label").style.color="rgb("+r+","+g+","+b+")";
+document.getElementById("result_confidence").style.color="rgb("+r+","+g+","+b+")";
+img=document.getElementById("animal");
+if(results[0].label=="dog"){
+    img.src="dog.jfif"
+}
+else if(results[0].label=="cow"){
+    img.src="cow.jfif"
+}
+else{
+    img.src="cat.jpg"
+}
+}
+}
